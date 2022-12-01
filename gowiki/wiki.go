13038f2434c33434193908566c2b2c82618704cf
Chen Qixian <chenqixian@stu.pku.edu.cn>
2022-12-01 20:35:41 +0800

    feat: reconstruct with abstract

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -34,13 +34,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	fmt.Println(title)
-	p, _ := loadPage(title)
 	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-	t, _ := template.ParseFiles("view.html")
-	t.Execute(w, p)
+	p, _ := loadPage(title)
+	// t, _ := template.ParseFiles("view.html")
+	// t.Execute(w, p)
+	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -56,8 +61,9 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	// 	"</form>",
 	// 	p.Title, p.Title, p.Body,
 	// )
-	t, _ := template.ParseFiles("edit.html")
-	t.Execute(w, p)
+	// t, _ := template.ParseFiles("edit.html")
+	// t.Execute(w, p)
+	renderTemplate(w, "edit", p)
 }
 
 func main() {
@@ -68,6 +74,6 @@ func main() {
 	// http.HandleFunc("/", handler)
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	// http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
